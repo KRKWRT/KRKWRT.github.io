62e15ee1e1a9d579d6ae483f0bcc52fe19bad9e6
Kurokawa Ryotaro <31917@toyota.kosen-ac.jp>
2025-04-21 02:06:49 +0900

    fix base name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://KRKWRT.github.io',
-    base: 'myHome',
+    base: 'KRKWRT.github.io',
 });
